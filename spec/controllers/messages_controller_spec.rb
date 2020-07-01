de21420dee888d2e9527f72753a793471235d0d1
Joe <josephhull676@yahoo.co.uk>
2020-07-01 11:52:46 +0100

    Began fixing controller tests. Messages controller test still broken and tests for get requests to dashboard if a user is logged out broken

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,12 +4,11 @@ RSpec.describe MessagesController, type: :controller do
   let(:mentor) { create(:mentor) }
   let(:student) { create(:student) }
   let(:conversation) { create(:conversation) }
-  let(:message) { create(:message) }
-  describe "GET /conversations" do
+  describe "GET /messages/index" do
     context "after a mentor signs in" do
       before { sign_in mentor }
       it "returns http success" do
-        conversation_messages_path(:conversation)
+        conversation_messages_path(conversation.id)
         expect(response).to have_http_status(:success)
       end
     end
